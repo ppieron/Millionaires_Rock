5c5263fb70f6ef48e29cd72ae66fa0fdb80740a7
ppieron <piotrek.pieron@gmail.com>
2023-02-22 22:53:40 +0100

    Questions moved to separate module

---
+++ Questions.py
@@ -0,0 +1,79 @@
+questions_level1 = ['Who is a frontman of a legendary rock band - The Rolling Stones:',
+                    'Slash is a famous:',
+                    'Curt Cobain was a frontman of:',
+                    '"Highway to Hell" is a signature song of:',
+                    'Which rock band has a lighting sign in its logo:',
+                    'The Beatles were an rock band from:',
+                    'From artists listed below, who was not a singer:',
+                    'Which rock band is famous for a song "We Will Rock You:"'
+                    ]
+
+answers_level1 = [['A. Bon Scott', 'B. Mick Jagger', 'C. Axl Rose', 'D. Ozzy Osbourne'],
+                  ['A. Singer', 'B. Drummer', 'C. Guitarist', 'D. Pianist'],
+                  ['A. Nirvana', 'B. Aerosmith', 'C. Black Sabbath', 'D. ACDC'],
+                  ['A. Guns n Roses', 'B. Marylin Mason', 'C. ACDC', 'D. The Rolling Stones'],
+                  ['A. ACDC', 'B. The Offspring', 'C. Disturbed', 'D. Korn'],
+                  ['A. USA', 'B. France', 'C. Scotland', 'D. England'],
+                  ['A. Freddie Mercury', 'B. Lenny Kravitz', 'C. Mick Jagger', 'D. Brian May'],
+                  ['A. Queen', 'B. Behemoth', 'C. Red Hot Chilli Peppers', 'D. The Beatles']
+                  ]
+
+correct_answers_level1 = ["B. Mick Jagger", "C. Guitarist", "A. Nirvana", "C. ACDC", "A. ACDC", "D. England",
+                          'D. Brian May', 'A. Queen']
+
+questions_level2 = ["What's the name of the latest album of ACDC:",
+                    'Ozzy Osbourne is not an author of:',
+                    'Who is the frontman of Korn:',
+                    'Which song performed by Marylin Manson is a cover: ',
+                    'In clip for which song members of Queen are dressed like women: ',
+                    'Father of an actress Liv Tyler is a frontman of which rock band: ',
+                    'The famous scene in which Axl rose gets married comes from the clip to:',
+                    '"Californication" is a title of TV series with David Duchovny and a song of:',
+                    'The famous rock ballad "Nothing Else Matters" is written by:',
+                    'Who is the frontman of Iron Maiden'
+                    ]
+
+answers_level2 = [['A. Back in Black', "B. Power Up", 'C. Highway to Hell', 'D. High Voltage'],
+                  ['A. Paranoid', 'B. Iron Man', 'C. T.N.T.', 'D. Crazy Train'],
+                  ['A. Jonathan Davis', 'B. James Hetfield', 'C. Brian Johnson', 'D. Lenny Kravitz'],
+                  ['A. Beautiful People', 'B. mObscene', 'C. Antichrist Superstar', 'D. Tainted Love'],
+                  ['A. We Will Rock You', 'B. Bohemian Rhapsody', 'C. I Want to Break Free', 'D. Hammer to Fall'],
+                  ['A. Guns n Roses', 'B. Red Hot Chilli Peppers', 'C. Metallica', 'D. Aerosmith'],
+                  ['A. Paradise City', "B. Yesterdays", 'C. November Rain', 'D. You Could Be Mine'],
+                  ['A. The Offspring', 'B. Red Hot Chilli Peppers', 'C. Ozzy Osbourne', 'D. The Rolling Stones'],
+                  ['A. The Rolling Stones', 'B. Black Sabbath', 'C. Rammstein', 'D. Metallica'],
+                  ['A. Bruce Dickinson', 'D. Brian Johnson', 'C. Mick Jagger', 'D. Ozzy Osbourne']
+                  ]
+
+correct_answers_level2 = ["B. Power Up", 'C. T.N.T.', 'A. Jonathan Davis', 'D. Tainted Love', 'C. I Want to Break Free',
+                          'D. Aerosmith', 'C. November Rain', 'B. Red Hot Chilli Peppers', 'D. Metallica',
+                          'A. Bruce Dickinson']
+
+questions_level3 = ["Who was a vocalist on ACDC's album 'Back in Black': ",
+                    'Who was the first lead vocalist of Nightwish:',
+                    "Which Ozzy Osbourne's guitarist died in a plane crash:",
+                    "What fairy tale does the music video for the Rammstein's song 'Sonne' refer to",
+                    'In which TV series Marlyin Manson appeared as himself:',
+                    'Which song by Korn was first: ',
+                    'Axl Rose was a frontman of Guns n Roses and: ',
+                    'Which rock band covered "The Sound of Silence":',
+                    'Take me down to the Paradise City, where: ',
+                    'Greta van Fleet is often compared to:',
+                    ]
+
+answers_level3 = [['A. Bon Scott', 'B. Brian Johnson', 'C. Axl Rose', 'D. Peter Clack'],
+                  ['A. Anette Olzon', 'B. Floor Jansen', 'C. Tarja Turunen', 'D. Alissa White-Gluz'],
+                  ['A. Randy Rhoads', 'B. Zakk Wydle', 'C. Steve Vai', 'D. Lemmy Kilmister'],
+                  ['A. Cindirella', 'B. Snow White', 'C. Hänsel und Gretel', 'D. The Little Match Girl'],
+                  ['A. Sons of Anarchy', 'B. Callifornication', 'C. The Office', 'D. House of Cards'],
+                  ['A. Falling Away from Me', 'B. Coming Undone', 'C. Freak On a Leash', 'D. Here to Stay'],
+                  ['A. The Rolling Stones', 'B. Iron Maiden', 'C. Aerosmith', 'D. ACDC'],
+                  ['A. Marylin Manson', 'B. Guns n Roses', 'C. Disturbed', 'D. Motorhead'],
+                  ['A. The grass is green and the girls are pretty.', "B. The love is free and the life ain't shitty.",
+                   "C. The sky is blue and and girls are pretty.", 'D. The drugs are free and they make us witty'],
+                  ['A. Guns n Roses', 'B. Led Zeppelin', 'C. Deep Purple', 'D. Pink FLoyd'],
+                  ]
+
+correct_answers_level3 = ['B. Brian Johnson', 'C. Tarja Turunen', 'A. Randy Rhoads', 'B. Snow White',
+                          'B. Callifornication', 'C. Freak On a Leash', 'D. ACDC', 'C. Disturbed',
+                          'A. The grass is green and the girls are pretty.', 'B. Led Zeppelin']
